2dd08552c33e29becafb5f3469f4e6fbb3682d15
Jithendra Gannavarapu <gannavarapujithendra@gmail.com>
2025-12-07 18:29:25 +0000

    adding the input output redirections(<,>) in shell script

--- shell.c
+++ shell.c
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <unistd.h>    
 #include <sys/wait.h>
-
+#include <fcntl.h>
  int generate_tokens(char *str, char *argv[]){
     int argc = 0;
     char *token = strtok(str, " ");
@@ -16,10 +16,41 @@
     argv[argc] = NULL;
     return argc;
  }
+int redirection_handling(char *argv[], int argc,char **file_in, char **file_out){
+    int result=0;
+    *file_in=NULL;
+    *file_out=NULL;
+    for(int i=0;i<argc;i++){
+        if(strcmp(argv[i],"<")==0){
+            if(i+1>=argc){
+                printf("ERROR::file name is not there after '<' ");
+                return -1;
+            }
+            *file_in = argv[i+1];
+            i=i+1;
+        }
+       else if(strcmp(argv[i],">")==0){
+            if(i+1>=argc){
+                printf("ERROR::file name is not there after '>' ");
+                return -1;
+            }
+            *file_out = argv[i+1];
+            i=i+1;
+        }
+        else{
+            argv[result]=argv[i];
+            result=result+1;
+        }
+    }
+    argv[result]=NULL;
+    return result;
+} 
 int main() {
     char *line = NULL;   
     size_t len = 0;     
     char *argv[50];
+    char *file_in=NULL;
+    char *file_out=NULL;
     while (1) {
         printf("mini-shell> Please type your command: ");
         fflush(stdout);  
@@ -38,9 +69,11 @@ int main() {
             continue;
         }
         int argc = generate_tokens(line,argv);
-        
-        if(argc==0)
-         continue;
+        argc = redirection_handling(argv,argc,&file_in,&file_out);
+        if(argc<=0){
+            printf("Enter a valid command");
+            continue;
+        }
         if(strcmp(argv[0],"exit")==0)
          break;
         if(strcmp(argv[0],"cd")==0){
@@ -58,6 +91,20 @@ int main() {
             continue;
         }
         else if(val==0){
+
+            if (file_out != NULL) {
+    		int fd = open(file_out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    		if (fd < 0) { perror("open"); exit(1); }
+    		dup2(fd, 1);   
+    		close(fd);
+		}
+
+	     if (file_in != NULL) {
+    		int fd = open(file_in, O_RDONLY);
+    		if (fd < 0) { perror("open"); exit(1); }
+    		dup2(fd, 0);   
+    		close(fd);
+		}
             execvp(argv[0],argv);
             perror("execvp");
         }
